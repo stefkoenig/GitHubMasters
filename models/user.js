79ea1693d0fcaf5ed1805b16f304d4f565056ac3
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-13 12:27:46 -0800

    added a user model which uses a mongoose schema

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+
+var userSchema = new mongoose.Schema({
+  user_name: String
+  ,email: {type: String, required: true, unique: true}
+  ,friends: []
+  ,age: Number
+})
+
+userSchema.methods.info = function(){
+  console.log('My username is: ' + this.user_name + '. I am' + this.age + ' years young.')
+}
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
