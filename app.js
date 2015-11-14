9b4ceae1d3a8d40b77878f36b9230d74a267e767
Ted Shumway <shumway.ted@gmail.com>
2015-11-13 17:09:51 -0800

    app.js is updated

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 var express = require('express')
+,			app		= express()
+,	 	logger	= require('morgan')
+,bodyParser = require('body-parser')
+, mongoose	= require('mongoose')
+
+//establishes connection to MongoDB
+mongoose.connect('mongodb://colino:banana1@ds053774.mongolab.com:53774/colino') //location of db
+
+//track requests made to the app
+app.use(logger('dev')) //middleware software that runs before other software. in this instance we want to log data on the user before they visit the page.
+
+//makes json object available in requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+//create the root route
+app.get('/', function(req,res){
+	res.send('Welcome to the Hizzy')
+})
+
+//import user routes
+var userRoutes = require('./routes/user_routes.js')
+app.use('/users', userRoutes)
+app.listen(3000)
+console.log('Server is running on 3000')
